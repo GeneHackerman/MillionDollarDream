b0240444c4279c2e78f2c6375c4516811eaa592d
Mariana Marques <65464431+TheQueenofEverything@users.noreply.github.com>
2022-01-28 01:30:05 -0100

    Update YahooFinanceAPI_script.js

--- assets/js/YahooFinanceAPI_script.js
+++ assets/js/YahooFinanceAPI_script.js
@@ -1,20 +1,6 @@
 // --------------------------- YAHOO FINANCE API ------------------------------
 
 
-//Fetch Stock data from four symbols using the yahoo finance API, jsons it into an object, then uses the object in stocklist function 
-
-
-// NOTE: V1 NOT WORKING ----- USE V2  || 
-
-fetch(“https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2020-10-14?adjusted=true&apiKey=mNgHEk6aL2MGcLCcUpq470Xhd4l8FCJK")
-.then(function(response){
-    return response.jason();
-    
-
-    
-))
-
-
 
 const data = await yahooStockPrices.getCurrentData('AAPL');
 console.log(data); // { currency: 'USD', price: 132.05 }
