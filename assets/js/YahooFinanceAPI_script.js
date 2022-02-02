dae4bb776a32b1a37fde4e293da0a5b88af6d98d
hthomp15 <hailethomp@gmail.com>
2022-02-01 21:11:05 -0500

    add stock search

--- assets/js/YahooFinanceAPI_script.js
+++ assets/js/YahooFinanceAPI_script.js
@@ -1,25 +1,21 @@
-
+// This needs to be the search input
+var searchTerm = "AAPL"
 // Fetch Simple Data 
-var getStockData  = ()=> {
-    var myKey="T2UJTKZI47JLWJ0G"
-    var stock = "AAPL";
-    var stockLink = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stock}&apikey=${myKey}`;
+var getStockData  = (stock)=> {
+        var myKey="T2UJTKZI47JLWJ0G"
+        var stockLink = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stock}&apikey=${myKey}`;
     
-    fetch(stockLink).then(async function(response) {
-       if (response.ok) {
-           const data = await response.json();
-        //    console.log(data);
-            console.log(data)
-        } else {
-            alert('Error: Search not found');
-        };
-     });
+        fetch(stockLink).then(async function(response) {
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data)
+             } else {
+                 alert('Error: Search not found');
+             };
+          }); 
 };
-getStockData()
 
-
-
-
+getStockData(searchTerm);
 
 
 
